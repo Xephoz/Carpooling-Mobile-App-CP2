19fce87875e69c4b0ec4e0337f93dba326a590e0
Xephoz <46962486+Xephoz@users.noreply.github.com>
2025-07-06 03:21:11 +0800

    Minor UI changes to driver_create, including moving button to bottom.

--- app/src/main/java/com/example/capstone2/main/MainActivity.kt
+++ app/src/main/java/com/example/capstone2/main/MainActivity.kt
@@ -3,19 +3,19 @@ package com.example.capstone2.main
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
+import androidx.core.view.WindowCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.capstone2.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         // Setup NavController with BottomNavigationView
         val navHostFragment = supportFragmentManager
