1c58595f430e0bcba248bf724a55c227eb1fdd35
Xephoz <46962486+Xephoz@users.noreply.github.com>
2025-07-06 05:09:55 +0800

    Minor UI changes to auth layouts

--- app/src/main/java/com/example/capstone2/auth/AuthActivity.kt
+++ app/src/main/java/com/example/capstone2/auth/AuthActivity.kt
@@ -1,8 +1,13 @@
 package com.example.capstone2.auth
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.example.capstone2.R
 
 class AuthActivity : AppCompatActivity() {
@@ -10,7 +15,13 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_auth)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.auth_nav_host_fragment)) { view, insets ->
+            val bottom = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+            view.updatePadding(bottom = bottom)
+            insets
+        }
     }
 }
